dcc4846a693072ff0370728692f9d1367488436c
tade18 <tade.18@seznam.cz>
2025-09-11 11:20:39 +0200

    ahoj

--- src/4rocnik/Maturita/Files/Movie.cs
+++ src/4rocnik/Maturita/Files/Movie.cs
@@ -4,6 +4,7 @@
     public string Genre { get; set; }
     public string LeadStudio { get; set; }
     public double AudienceScore { get; set; }
+    //
     public double Profitability { get; set; }
     public double RottenTomatoes { get; set; }
     public double WorldwideGross { get; set; }
